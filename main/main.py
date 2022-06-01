6a6f734f889aed79f120e33cd4dbc9f117d9f370
thibaultanani <thuny.ta@gmail.com>
2022-06-01 23:19:35 +0200

    Update requirements.txt

--- main/main.py
+++ main/main.py
@@ -1,6 +1,11 @@
 import numpy as np
 import sys
+import os
 import warnings
+
+conf_path = os.getcwd()
+sys.path.append(os.path.dirname(conf_path))
+sys.path.append(os.path.join(os.path.dirname(conf_path), 'classes'))
 
 from classes.BPLDE import Differential
 from classes.Data import Data
@@ -8,7 +13,6 @@ from classes.Data import Data
 warnings.filterwarnings('ignore')
 
 np.set_printoptions(threshold=sys.maxsize)
-
 
 if __name__ == '__main__':
     data = Data(dataset="heart", target="DEATH_EVENT")
